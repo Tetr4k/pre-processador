cdaeb97a244ac6e1dda084c95bb32a5b8150d94e
Gabriel <g2012ribeiro@gmail.com>
2021-07-10 14:53:33 -0300

    Adiciona multi "parametragem"
    São processados mais de um arquivo
    Adicionado comentarios

--- processa.py
+++ processa.py
@@ -1,12 +1,42 @@
 import sys
 
-nomeArquivo = sys.argv[1]
+nomesArquivos = sys.argv
+nomesArquivos.pop(0)
 
-arquivo = open(nomeArquivo, 'r')
-print(arquivo.readlines())
+for nomeArquivo in nomesArquivos:
+    arquivo = open(nomeArquivo, 'r')
+    print(arquivo.readlines())
 
-#Includes e defines
+    
+    arquivo.close()
+
+#Includes
+
+#Includes de Includes?
+
+# Enquanto tiver include no arquivo{
+#   busca o primeiro include
+#   usa ""? -> procura na pasta do projeto
+#   usa <>? -> procura na pasta do gcc
+#   inclui o conteudo
+# }
+
+#defines 
+# Sobre os defines, é importante considerar macros também.
+# como assim macros?????
 
 #Remover comentarios
+
+# Busca //
+# Se encontrou{
+#   remove conteudo entre // e o /n
+#   Busca //
+# }
+#
+# Busca /*
+# Se encontrou{
+#   remove conteudo até encontrar */
+#   Busca /*
+# }
 
 #Tira quebras de linha e espaços
