6971757c8ac87c52f68223694b99488c9ba7b5bb
Gabriel <g2012ribeiro@gmail.com>
2021-07-11 21:05:04 -0300

    Insere conteúdo dos includes

--- processa.py
+++ processa.py
@@ -3,25 +3,39 @@ def fazIncludeAspas(codigo):
     for linha in codigo:
         include = re.search("#include\s*\"[\d\w]*\.[ch]\"\s*\n", linha)
         if include and include.group() == linha:
-            arquivo = re.search("(?<=\")[\d\w]*\.[ch](?=\")", include.group()).group()
-            print(arquivo)#Fazer -> buscar arquivo no compilador e incluir
+            nomeArquivo = re.search("(?<=\")[\d\w]*\.[ch](?=\")", include.group()).group()
+            arquivo = open(nomeArquivo, 'r')
+            if not arquivo:
+                #Procura na pasta do compilador
+                print("pra tirar erro")
+            conteudo = arquivo.readlines()
+            arquivo.close()
+            for linha2 in codigo:
+                conteudo.append(linha2)
+            codigo = conteudo
     return codigo
-def fazIncludeMQMQ(codigo):
-    for linha in codigo:
-        include = re.search("#include\s*<[\d\w]*\.[ch]>\s*\n", linha)
-        if include and include.group() == linha:
-            arquivo = re.search("(?<=<)[\d\w]*\.[ch](?=>)", include.group()).group()
-            print(arquivo)#Fazer -> buscar arquivo no projeto e incluir
-    return codigo
+# def fazIncludeMQMQ(codigo):
+#     for linha in codigo:
+#         include = re.search("#include\s*<[\d\w]*\.[ch]>\s*\n", linha)
+#         if include and include.group() == linha:
+#             nomeArquivo = re.search("(?<=<)[\d\w]*\.[ch](?=>)", include.group()).group()
+#             arquivo = open(nomeArquivo, 'r')#Arrumar endereço
+#             conteudo = arquivo.readlines()
+#             arquivo.close()
+#             conteudo = fazIncludeMQMQ(conteudo)
+#             conteudo.append(codigo)
+#             codigo.clear()
+#             codigo = conteudo
+#     return codigo
 def tiraComentarioLinha(linha):                         #Funcao para remover comentarios de linha
     novaLinha = re.sub("//(.*)\\n", "", linha)          #Substitui comentario de linha por "" usando regex
     return novaLinha                                    #Retorna nova linha
 def tiraQuebras(codigo):#Fazer -> não feito
     #texto = re.sub()
     return codigo
-def tiraComentarioParagrafo(codigo):#Fazer -> possiveis erros
-    codigo = re.sub("/\*(.*)\*/", "", codigo)
-    return codigo
+# def tiraComentarioParagrafo(codigo):#Fazer -> possiveis erros
+#     codigo = re.sub("/\*(.*)\*/", "", codigo)
+#     return codigo
 sistema = platform.system()                             #Identifica sistema
 os.system("mkdir backup")
 nomesArquivos = sys.argv                                #Acessa parametros passados
@@ -40,11 +54,11 @@ for nomeArquivo in nomesArquivos:                       #Faz o pre-processamento
 
     
     #Manipulacao do buffer
-    #Includes compilador 
-    buffer = fazIncludeAspas(buffer)                       #Includes projeto 
-    buffer = fazIncludeMQMQ(buffer)
+    
+    buffer = fazIncludeAspas(buffer)                       #Includes ""
+    #buffer = fazIncludeMQMQ(buffer)                       #Includes <> 
+    
     #Defines
-
     buffer = map(tiraComentarioLinha, buffer)           #Remove comentario do tipo "//" de cada linha
 
     #buffer = tiraQuebras(buffer)                       #Remove "/n"s
