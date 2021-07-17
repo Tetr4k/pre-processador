5167263a38ee11be143a6eb105367febcbfdde4c
Gabriel <g2012ribeiro@gmail.com>
2021-07-17 19:20:34 -0300

    Cria tiraStrings e botaStrings

--- processa.py
+++ processa.py
@@ -29,24 +29,20 @@ def fazIncludeAspas(codigo):                                                    
                 bufferIncludes.append(nomeArquivo)                                          #Inclui arquivo na lista de arquivos incluidos
                 try:
                     try:
-                        arquivo = open(nomeArquivo, 'r')                                        #Abre arquivo se existir
-                    except:                                                                     #Se gerar erro tenta abrir do compilador
-                        if sistema == "Windows":
-                            arquivo = open("c:\\mingw\\include\\"+nomeArquivo, 'r')             #Se o sistema for Windows procura a partir de C:
-                        else:
-                            arquivo = open("\\usr\\include\\"+nomeArquivo, 'r')                 #Se o sistema for Linux/MAC procura a partir da raiz
+                        arquivo = open(nomeArquivo, 'r')                                    #Abre arquivo se existir
+                    except:                                                                 #Se gerar erro tenta abrir do compilador
+                        arquivo = abreCompilador(nomeArquivo)
                 except:
-                    buffer.append(linha)                                                    #Mantém linha caso arquivo não seja encontrado
+                    print("Arquivo", nomeArquivo, "não podê ser encontrado.")
                     continue
                 conteudo = leArquivo(arquivo)                                               #Pega conteudo do arquivo com includes
                 arquivo.close()                                                             #Fecha arquivo
-                for linhaConteudo in conteudo:                                              #Copia conteudo do include para o buffer
-                    buffer.append(linhaConteudo)
+                buffer.append(conteudo)                                                     #Copia conteudo do include para o buffer
         else:                                                                               #Se não tiver ou não for valido
             buffer.append(linha)                                                            #Mantém a linha
     return buffer                                                                           #Retorna codigo resultado
 
-def fazIncludeAngular(codigo):      #Função para resolver includes com Colchetes Angulares
+def fazIncludeAngular(codigo):                                                              #Função para resolver includes com Colchetes Angulares
     buffer = []                                                                             #Buffer vazio
     while codigo:                                                                           #Enquanto existem linhas não processadas
         linha = codigo[0]                                                                   #Faz uma copia da primeira linha
@@ -57,62 +53,72 @@ def fazIncludeAngular(codigo):      #Função para resolver includes com Colchet
             if not nomeArquivo in bufferIncludes:                                           #Se arquivo ainda não foi incluido
                 bufferIncludes.append(nomeArquivo)                                          #Inclui arquivo na lista de arquivos incluidos
                 try:
-                    if sistema == "Windows":
-                        arquivo = open("c:\\mingw\\include\\"+nomeArquivo, 'r')                 #Se o sistema for Windows procura a partir de C:
-                    else:
-                        arquivo = open("\\usr\\include\\"+nomeArquivo, 'r')                     #Se o sistema for Linux/MAC procura a partir da raiz
+                    arquivo = abreCompilador(nomeArquivo)    
                 except:
-                    buffer.append(linha)                                                    #Mantém linha caso arquivo não seja encontrado
+                    print("Arquivo", nomeArquivo, "não podê ser encontrado.")
                     continue
                 conteudo = leArquivo(arquivo)
                 arquivo.close()                                                             #Fecha arquivo
-                for linhaConteudo in conteudo:                                              #Copia pro buffer conteudo do include
-                    buffer.append(linhaConteudo)
+                buffer.append(conteudo)                                                     #Copia conteudo do include para o buffer
         else:                                                                               #Se não tiver ou não for valido
             buffer.append(linha)                                                            #Mantém a linha
     return buffer                                                                           #Retorna codigo resultado
 
-def tiraComentarioLinha(linha):     #Funcao para remover comentarios de linha
-    '''tem problema com """s -> VALIDAR """s'''
-    return re.sub("//.*$", "\n", linha)                                                             #Substitui comentario de linha por "" usando regex e retorna
+def preprocessa(buffer):#Função para pre-processar o codigo
+    #Para cada include com Aspas no arquivo, se o arquivo incluido existir e estiver na pasta, copia seu conteudo, se não estiver na pasta, procura no compilador, se o arquivo não existir, passa para o proximo.
+    #buffer = fazIncludeAspas(buffer)            #Includes ""
 
-def tiraComentarioParagrafo(linha): #Funcao para remover comentarios de paragrafo
-    '''tem problema com """s -> VALIDAR """s'''
-    procura = re.search("\/\*.*?\*\/", linha)
-    if procura:
-        print(procura)
-    return re.sub("\/\*.*?\*\/", "", linha)                                                         #Substitui comentario de paragrafo por "" usando regex e retorna
-
-def tiraEspacos(linha):             #Funcao para remover espaços não uteis
-    '''tem problema com """s -> validar """s'''
-    return re.sub("\s*(?=[-+*\/<>=,&|!(){}\[\];])|(?<=[-+*\/<>=,&|!(){}\[\];])\s*", "", linha) #Substitui espaços não uteis por "" usando regex e retorna
-
-def tiraQuebras(linha):             #Função para remover quebras de linha
-    return re.sub("\\n$", "", linha)                                                                #Substitui quebras de linha por "" usando regex e retorna
-
-def preprocessa(buffer):            #Função para pre-processar o codigo
-#Para cada include com Aspas no arquivo, se o arquivo incluido existir e estiver na pasta, copia seu conteudo, se não estiver na pasta, procura no compilador, se o arquivo não existir, mantém o erro de sintaxe.
-    buffer = fazIncludeAspas(buffer)            #Includes ""
-
-#Para cada include com Colchetes angulares no arquivo, se o arquivo incluido existir e estiver no compilador, copia seu conteudo, se o arquivo não existir, mantém o erro de sintaxe.
-    buffer = fazIncludeAngular(buffer)          #Includes <> 
+    #Para cada include com Colchetes angulares no arquivo, se o arquivo incluido existir e estiver no compilador, copia seu conteudo, se o arquivo não existir, passa para o proximo.
+    #buffer = fazIncludeAngular(buffer)          #Includes <> 
     
-    '''LOGICA DEFINES'''
-    #                                           #Defines
+    '''volta com \n mas no arquivo'''
+    bufferStrings = []
+    #def tiraStrings(linha):
+    #    string = re.search("\".*\"", linha)
+    #    if string:
+    #        bufferStrings.append(string.group())
+    #    return re.sub("\".*\"", "#str"+str(len(bufferStrings)-1), linha)
+    #buffer = map(tiraStrings, buffer)
+    #def botaStrings(linha):
+    #    string = re.search("(?<=#str)\d*", linha)
+    #    if string:
+    #        return re.sub("#str\d*", bufferStrings[int(string.group())], linha)
+    #    return linha
+    #buffer = map(botaStrings, buffer)
 
-    '''Tira do \n até o // atras'''   
-#Para cada linha, se a linha tem // valido(fora de "" validas), apaga conteudo até \n
-    buffer = map(tiraComentarioLinha, buffer)   #Remove comentario do tipo "//" de cada linha
+    '''Para cada linha do código, se a linha contem um define, caso o define seja uma função, irá remover as chaves, caso tenha, e depois irá percorrer o código novamente.
+    Se Achar alguma ocorrência do define no código, irá pegar a expressão, substituir os valores nas variáveis, e então substituir a chamada do define pela expressão.
+    Depois, adicionará a linha em um buffer, que posteriormente será retornado.
+    Caso não seja uma função, apenas substituirá o valor onde houver a a chamada. Depois, adicionará a linha alterada no buffer.
+    Após percorrer todo o código, retornará o buffer'''
 
-#Para cada linha, remove o ultimo "\n"
-    buffer = map(tiraQuebras, buffer)           #Remove "/n" de cada linha, defines ja estarão resolvidos
-
-#Para cada linha, remove espaços em volta de -+*\/<>=,&|!(){}[]
-    buffer = map(tiraEspacos, buffer)
+    buffer = trataDefine(buffer)                                #Defines 
+    print(buffer)
     
-    '''LOGICA REMOVER /**/'''
-    #buffer = map(tiraComentarioParagrafo, buffer)   #Remove comentario do tipo "/*"
-    
+    def tiraComentarioLinha(linha):     #Funcao para remover comentarios de linha
+        #Substitui comentario de linha por "" usando regex e retorna
+        return re.sub("//.*$", "\n", linha)
+    #Para cada linha, se a linha tem // valido(fora de "" validas), apaga conteudo até \n
+    #buffer = map(tiraComentarioLinha, buffer)   #Remove comentario do tipo "//" de cada linha
+
+    def tiraEspacos(linha):     #Funcao para remover espaços não uteis
+        #Substitui espaços não uteis por "" usando regex e retorna
+        return re.sub("\s*(?=[-+*\/<>=,&|!(){}\[\];])|(?<=[-+*\/<>=,&|!(){}\[\];])\s*", "", linha)
+    #Para cada linha, remove espaços em volta de -+*\/<>=,&|!(){}[]
+    #buffer = map(tiraEspacos, buffer)
+
+    def tiraQuebras(linha):     #Função para remover quebras de linha
+        #Substitui quebras de linha por "" usando regex e retorna
+        return re.sub("\\n$", "", linha)
+    #Para cada linha, remove o ultimo "\n"
+    #buffer = map(tiraQuebras, buffer)           #Remove "/n" de cada linha, defines ja estarão resolvidos
+
+    #'''LOGICA REMOVER /**/'''
+    #def tiraComentarioParagrafo(linha): #Funcao para remover comentarios de paragrafo
+    #    return re.sub("/\*.*?\*/", "", linha)                                                         #Substitui comentario de paragrafo por "" usando regex e retorna
+    #buffer = tiraComentarioParagrafo(''.join(buffer))  #Remove comentario do tipo "/*"
+    '''Voltar strings'''
+    bufferStrings.clear()                       #Limpa buffer de strings
     bufferIncludes.clear()                      #Limpa buffer de includes
     return buffer                               #Retorna conteudo após manipulação
 
