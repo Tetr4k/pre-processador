1133a11ca5bdd7b6bc13e8bd9bab46b2932d31da
Gabriel <g2012ribeiro@gmail.com>
2021-07-18 00:35:59 -0300

    Cria trataDefines

--- processa.py
+++ processa.py
@@ -66,27 +66,73 @@ def fazIncludeAngular(codigo):                                                  
             buffer.append(linha)                                                            #Mantém a linha
     return list(buffer)                                                                           #Retorna codigo resultado
 
-def preprocessa(buffer):#Função para pre-processar o codigo
-    #Para cada include com Aspas no arquivo, se o arquivo incluido existir e estiver na pasta, copia seu conteudo, se não estiver na pasta, procura no compilador, se o arquivo não existir, passa para o proximo.
-    #buffer = fazIncludeAspas(buffer)            #Includes ""
+def trataDefine(codigo):
+    #Ver quais são os defines
+    #jogar instruções de define em um vetor
 
-    #Para cada include com Colchetes angulares no arquivo, se o arquivo incluido existir e estiver no compilador, copia seu conteudo, se o arquivo não existir, passa para o proximo.
-    #buffer = fazIncludeAngular(buffer)          #Includes <> 
-    
+    #Para cada instrução no vetor
+    #percorre o codigo substituindo
+    defines = []
+    buffer = []
+    for linha in codigo:
+        define = re.search("#define\s*.*\s*(?=(\n|//|/*))", linha)                                 #Expressão regular para verificar se tem define na linha
+
+        #if define:
+        #    linha = re.sub("#define\s*.*\s*(?=(\n|//|/*))", "", linha)
+        if define and define.group() == linha:                                                  #Se tiver define na linha e for valido
+            conteudoDefine = re.search("(?<=\s).*$", define.group())                             #Pega o conteúdo do define (nome e valor)
+            nomeDefine = re.search(".*(?=\s)", conteudoDefine.group())                           #Pega o nome do define
+            if nomeDefine and "(" in nomeDefine.group() and ")" in nomeDefine.group():                            #Se define for Macro
+                valorDefine = re.search("(?<=\)\s).*", conteudoDefine.group())
+                if "{" in valorDefine.group() and "}" in valorDefine.group():                                          #Se tiver chaves
+                    valorFuncDefine = re.search("(?<={).*(?=})", valorDefine.group())           #Tira chaves
+                else:
+                    valorFuncDefine = valorDefine
+                nomeDefineSemParametro = re.search(".*(?=\()", nomeDefine.group())                      #Pega apenas o nome da função sem os parametros
+                parametros = re.search("(?<=\().*(?=\))", conteudoDefine.group())                          #Pega apenas a parte de dentro dos parênteses da função define
+                parametros = parametros.group().replace(" ", "")
+                parametros = parametros.split(",")
+                defines.append([nomeDefineSemParametro.group(), valorFuncDefine.group(), parametros])    
+            else:                                                                               #Se não for função (não tem parênteses)
+                valorDefine = re.search("(?<=\s).*", conteudoDefine.group())                             #Pega o valor do define
+                defines.append([nomeDefine.group(), valorDefine.group()])
+    while codigo:
+        linha = codigo[0]
+        codigo.pop(0)
+        for define in defines:
+            nome = re.search("(?<=[^a-zA-Z0-9])"+define[0]+"(?=[^a-zA-Z0-9])", linha)
+            if nome:
+                if len(define) == 2:
+                    linha = linha.replace(define[0], define[1])
+                elif len(define) == 3:
+                    valorParametros = re.search("(?<=[\W\D])"+define[0]+"\(.*,.*\)", linha)
+                    valorParametros = re.search("(?<=\().*(?=\))", valorParametros.group())
+                    valorParametros = valorParametros.group().replace(" ", "")
+                    valorParametros = valorParametros.split(",")
+                    funcaoComValores = define[1]
+                    for i in range(len(valorParametros)):
+                        funcaoComValores = funcaoComValores.replace(define[2][i], valorParametros[i])
+                    linha = linha.replace(re.search(define[0]+"\(.*,.*\)", linha).group(), funcaoComValores)
+                break
+        if not "define" in linha:
+            buffer.append(linha)
+    return buffer                                                                          #retorna o código com as alterações
+
+def preprocessa(buffer):    #Função para pre-processar o codigo
     '''volta com \n mas no arquivo'''
     bufferStrings = []
-    #def tiraStrings(linha):
-    #    string = re.search("\".*\"", linha)
-    #    if string:
-    #        bufferStrings.append(string.group())
-    #    return re.sub("\".*\"", "#str"+str(len(bufferStrings)-1), linha)
-    #buffer = map(tiraStrings, buffer)
-    #def botaStrings(linha):
-    #    string = re.search("(?<=#str)\d*", linha)
-    #    if string:
-    #        return re.sub("#str\d*", bufferStrings[int(string.group())], linha)
-    #    return linha
-    #buffer = map(botaStrings, buffer)
+    def tiraStrings(linha):
+        string = re.search("\".*\"", linha)
+        if string:
+            bufferStrings.append(string.group().replace("\\", "\\\\"))
+        return re.sub("\".*\"", "#str"+str(len(bufferStrings)-1), linha)
+    buffer = list(map(tiraStrings, buffer))
+
+    def tiraComentarioLinha(linha):     #Funcao para remover comentarios de linha
+        #Substitui comentario de linha por "" usando regex e retorna
+        return re.sub("//.*$", "\n", linha)
+    #Para cada linha, se a linha tem // valido(fora de "" validas), apaga conteudo até \n
+    buffer = list(map(tiraComentarioLinha, buffer))   #Remove comentario do tipo "//" de cada linha
 
     '''Para cada linha do código, se a linha contem um define, caso o define seja uma função, irá remover as chaves, caso tenha, e depois irá percorrer o código novamente.
     Se Achar alguma ocorrência do define no código, irá pegar a expressão, substituir os valores nas variáveis, e então substituir a chamada do define pela expressão.
@@ -94,34 +140,44 @@ def preprocessa(buffer):#Função para pre-processar o codigo
     Caso não seja uma função, apenas substituirá o valor onde houver a a chamada. Depois, adicionará a linha alterada no buffer.
     Após percorrer todo o código, retornará o buffer'''
 
-    buffer = trataDefine(buffer)                                #Defines 
-    print(buffer)
-    
-    def tiraComentarioLinha(linha):     #Funcao para remover comentarios de linha
-        #Substitui comentario de linha por "" usando regex e retorna
-        return re.sub("//.*$", "\n", linha)
-    #Para cada linha, se a linha tem // valido(fora de "" validas), apaga conteudo até \n
-    #buffer = map(tiraComentarioLinha, buffer)   #Remove comentario do tipo "//" de cada linha
+    buffer = trataDefine(buffer)                       #Trata os Defines
 
     def tiraEspacos(linha):     #Funcao para remover espaços não uteis
         #Substitui espaços não uteis por "" usando regex e retorna
-        return re.sub("\s*(?=[-+*\/<>=,&|!(){}\[\];])|(?<=[-+*\/<>=,&|!(){}\[\];])\s*", "", linha)
-    #Para cada linha, remove espaços em volta de -+*\/<>=,&|!(){}[]
-    #buffer = map(tiraEspacos, buffer)
+        return re.sub("\s+(?=[-+*\/<>=,&|!(){}\[\];:])|(?<=[-+*\/<>=,&|!(){}\[\];:])\s+", "\n", linha)
+    #Para cada linha, remove espaços em volta de caracteres especiais
+    #buffer = list(map(tiraEspacos, buffer))
+
+    def tiraTabulacoes(linha):
+        return re.sub("\t", "\n", linha)
+    #Para cada linha, remove tabulações
+    buffer = list(map(tiraTabulacoes, buffer))
+    
+    def tiraComentarioParagrafo(linha): #Funcao para remover comentarios de paragrafo
+        return re.sub("/\*.*?\*/", "", linha)                                                         #Substitui comentario de paragrafo por "" usando regex e retorna
+    #'''LOGICA REMOVER /**/'''
+    #buffer = tiraComentarioParagrafo(''.join(buffer))  #Remove comentario do tipo "/*"
+    
+    def botaStrings(linha):
+        string = re.search("(?<=#str)\d*", linha)
+        if string:
+            return re.sub("#str\d*", bufferStrings[int(string.group())], linha)
+        return linha
+    buffer = list(map(botaStrings, buffer))
+
+    #Para cada include com Aspas no arquivo, se o arquivo incluido existir e estiver na pasta, copia seu conteudo, se não estiver na pasta, procura no compilador, se o arquivo não existir, passa para o proximo.
+    #buffer = fazIncludeAspas(buffer)            #Includes ""
+
+    #Para cada include com Colchetes angulares no arquivo, se o arquivo incluido existir e estiver no compilador, copia seu conteudo, se o arquivo não existir, passa para o proximo.
+    #buffer = fazIncludeAngular(buffer)          #Includes <> 
 
     def tiraQuebras(linha):     #Função para remover quebras de linha
         #Substitui quebras de linha por "" usando regex e retorna
         return re.sub("\\n$", "", linha)
     #Para cada linha, remove o ultimo "\n"
-    #buffer = map(tiraQuebras, buffer)           #Remove "/n" de cada linha, defines ja estarão resolvidos
-
-    #'''LOGICA REMOVER /**/'''
-    #def tiraComentarioParagrafo(linha): #Funcao para remover comentarios de paragrafo
-    #    return re.sub("/\*.*?\*/", "", linha)                                                         #Substitui comentario de paragrafo por "" usando regex e retorna
-    #buffer = tiraComentarioParagrafo(''.join(buffer))  #Remove comentario do tipo "/*"
-    '''Voltar strings'''
+    #buffer = list(map(tiraQuebras, buffer))           #Remove "/n" de cada linha, defines ja estarão resolvidos
+    
     bufferStrings.clear()                       #Limpa buffer de strings
-    bufferIncludes.clear()                      #Limpa buffer de includes
     return buffer                               #Retorna conteudo após manipulação
 
 os.system("mkdir backup")                               #Cria pasta de backup
@@ -129,7 +185,7 @@ nomesArquivos = sys.argv                                #Acessa parametros passa
 nomesArquivos.pop(0)                                    #Remove primeiro parametro("processa.py")
 
 for nomeArquivo in nomesArquivos:                       #Faz o pre-processamento para cada arquivo passado por parametro. 
-    
+    bufferIncludes.clear()                          #Limpa buffer de includes
     if sistema == "Windows":
         os.system("copy "+nomeArquivo+" backup")        #Faz Backup do arquivo em Windows
     else:
@@ -141,7 +197,7 @@ for nomeArquivo in nomesArquivos:                       #Faz o pre-processamento
         arquivo.close()                                 #Fecha o arquivo
         codigo = preprocessa(codigo)                    #Faz o pre-processa do codigo
         arquivo = open(nomeArquivo, 'w')                #Abre arquivo para escrita
-        arquivo.writelines(codigo)                      #Escreve o conteudo do arquivo
+        arquivo.writelines(codigo)                      #Escreve o conteudo no arquivo
         arquivo.close()                                 #Fecha o arquivo
     except:
         continue                                        #Se gerar erro, passa para o proximo arquivo
