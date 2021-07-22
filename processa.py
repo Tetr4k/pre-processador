1b9bf50449584aa6a3b5707be6b5c7a9f64f3bd1
Gabriel <g2012ribeiro@gmail.com>
2021-07-22 10:33:50 -0300

    Comentarios

--- processa.py
+++ processa.py
@@ -1,83 +1,93 @@
 import sys, os, platform, re
-sistema = platform.system()                             #Informa qual o sistema
+
+sistema = platform.system()                             #Identifica qual é o sistema
+
 if not sistema == ("Windows" or "Linux" or "Darwin"):   #Valida sistema
     print("O sistema", sistema, "não é reconhecido.")
     exit()
 
-def leArquivo(arquivo):                                     #Função para ler conteudo do arquivo e tratar os includes dele
-    conteudoArquivo = arquivo.readlines()                   #Le conteudo
-    return preprocessa(conteudoArquivo)    #Pre-processa e retorna conteudo
+def fazIncludes(buffer):                #Função para fazer todos os includes
+    buffer = listaIncludeAspas(buffer)
+    buffer = listaIncludeAngular(buffer)
+    buffer = resolveIncludeAspas(buffer)
+    buffer = resolveIncludeAngular(buffer)
+    return buffer
 
-def abreCompilador(nomeArquivo):
+def leArquivo(arquivo):                 #Função para ler conteudo do arquivo e tratar os includes dele
+    conteudoArquivo = arquivo.readlines()  #Le conteudo do arquivo
+    return fazIncludes(conteudoArquivo)    #Faz includes do arquivo e retorna conteudo
+
+def abreCompilador(nomeArquivo):        #Função para abrir arquivo na pasta do Mingw
     if sistema == "Windows":
-        return open("c:\\mingw\\include\\"+nomeArquivo, 'r')         #Se o sistema for Windows procura a partir de C:
+        return open("c:\\mingw\\include\\"+nomeArquivo, 'r')    #Se o sistema for Windows procura a partir de C:
     else:
-        return open("\\usr\\include\\"+nomeArquivo, 'r')             #Se o sistema for Linux/MAC procura a partir da raiz          
+        return open("\\usr\\include\\"+nomeArquivo, 'r')        #Se o sistema for Linux/MAC procura a partir da raiz          
 
-includesAspas = []
-def listaIncludeAspas(buffer):
-    def encontra(linha):
-        include = re.search("^#include\s*\".*\.[ch]\"", linha)
-        if include:
-            nomeArquivo = re.search("(?<=\").*\.[ch](?=\")", include.group())
-            if nomeArquivo and nomeArquivo not in includesAspas:
-                includesAspas.append(nomeArquivo.group())
-            linha = re.sub("^#include\s*\".*\.[ch]\"", "", linha)
-        return linha
-    return list(map(encontra, buffer))
+includesAspas = []      #Lista de includes de Aspas
+def listaIncludeAspas(buffer):          #Função para gerar uma lista de includes com aspas a partir do arquivo.
+    def encontra(linha):                #Função para encontrar os includes no arquivo. Usada através do map.
+        include = re.search("^\s*#include\s*\".*\.[ch]\"", linha)                  #Procura por um include
+        if include:                                                             #Se encontrou
+            nomeArquivo = re.search("(?<=\").*\.[ch](?=\")", include.group())   #Busca o arquivo a ser incluido
+            if nomeArquivo.group() not in includesAspas:                        #Verifica se ja esta listado para ser incluido
+                includesAspas.append(nomeArquivo.group())                       #Caso não esteja listado para ser incluido, adiciona na lista
+            linha = re.sub("^\s*#include\s*\".*\.[ch]\"", "", linha)               #Remove o include da linha
+        return linha                                                            #Retorna linha modificada ou não modificada
+    return list(map(encontra, buffer))                                          #Aplica a função encontra para cada linhado buffer e retorna
 
-includesAngulares = []
-def listaIncludeAngular(buffer):
-    def encontra(linha):
-        include = re.search("^#include\s*<.*\.[ch]>", linha)
-        if include:
-            nomeArquivo = re.search("(?<=<).*\.[ch](?=>)", include.group())
-            if nomeArquivo and nomeArquivo not in includesAngulares:
-                includesAngulares.append(nomeArquivo.group())
-            linha = re.sub("^#include\s*<.*\.[ch]>", "", linha)
-        return linha
-    return list(map(encontra, buffer))
+includesAngulares = []  #Lista de includes de Aspas
+def listaIncludeAngular(buffer):        #Função para gerar uma lista de includes com aspas a partir do arquivo.
+    def encontra(linha):                #Função para encontrar os includes no arquivo. Usada através do map.
+        include = re.search("^\s*#include\s*<.*\.[ch]>", linha)                    #Procura por um include
+        if include:                                                             #Se encontrou
+            nomeArquivo = re.search("(?<=<).*\.[ch](?=>)", include.group())     #Busca o arquivo a ser incluido
+            if nomeArquivo.group() not in includesAngulares:                    #Verifica se ja esta listado para ser incluido
+                includesAngulares.append(nomeArquivo.group())                   #Caso não esteja listado para ser incluido, adiciona na lista
+            linha = re.sub("^\s*#include\s*<.*\.[ch]>", "", linha)                 #Remove o include da linha
+        return linha                                                            #Retorna linha modificada ou não modificada
+    return list(map(encontra, buffer))                                          #Aplica a função encontra para cada linhado buffer e retorna
 
-incluidos = []
-def resolveIncludeAspas(buffer):
-    novoBuffer = []
-    for include in includesAspas:
-        if include not in incluidos:
-            incluidos.append(include)
-            includesAspas.remove(include)
-            try:                                                                        #Abre arquivo se existir
+incluidos = []#Lista de arquivos ja incluidos
+def resolveIncludeAspas(buffer):        #Função para resolver includes com Aspas
+    novoBuffer = []                                                     #Lista com novas linhas
+    for include in includesAspas:                                       #Para cada include de includesAspas
+        if include not in incluidos:                                    #Caso o include não tenha sido incluido ainda
+            incluidos.append(include)                                   #Adiciona include a lista de includes ja incluidos
+            includesAspas.remove(include)                               #Remove include da lista de includesAspas
+            try:                                                                        
                 try:
-                    arquivo = open(include, 'r')                                    
-                except:                                                                 #Se gerar erro tenta abrir do compilador
-                    arquivo = abreCompilador(include)
+                    arquivo = open(include, 'r')                        #Abre arquivo se existir
+                except:                                                                
+                    arquivo = abreCompilador(include)                   #Se gerar erro tenta abrir do compilador
             except:
                 print("Arquivo", include, "não podê ser encontrado.")
                 continue
-            conteudo = leArquivo(arquivo)                                               #Pega conteudo do arquivo com includes
-            arquivo.close()                                                             #Fecha arquivo
-            for linha in conteudo:
-                novoBuffer.append(linha)                                                     #Copia conteudo do include para o buffer
-    novoBuffer.extend(buffer)
-    return novoBuffer
+            conteudo = leArquivo(arquivo)                               #Pega conteudo do arquivo com includes
+            arquivo.close()                                             #Fecha arquivo
+            for linha in conteudo:                                      #Para cada linha do arquivo
+                novoBuffer.append(linha)                                #Copia conteudo do include para o buffer
+    novoBuffer.extend(buffer)                                           #Adiciona o conteudo no inicio do codigo
+    return novoBuffer                                                   #Retorna o novo codigo
 
-def resolveIncludeAngular(buffer):
-    novoBuffer = []
-    for include in includesAngulares:
-        if include not in incluidos:
-            incluidos.append(include)
+def resolveIncludeAngular(buffer):      #Função para resolver includes com Colchetes angulares
+    novoBuffer = []                                                     #Lista com novas linhas
+    for include in includesAngulares:                                   #Para cada include de includesAngulares
+        if include not in incluidos:                                    #Caso o include não tenha sido incluido ainda
+            incluidos.append(include)                                   #Adiciona include a lista de includes ja incluidos
+            includesAngulares.remove(include)                           #Remove include da lista de includesAngulares
             try:
-                arquivo = abreCompilador(include)    
+                arquivo = abreCompilador(include)                       #Abre arquivo se existir
             except:
                 print("Arquivo", include, "não podê ser encontrado.")
                 continue
-            conteudo = leArquivo(arquivo)                                               #Pega conteudo do arquivo com includes
-            arquivo.close()                                                             #Fecha arquivo
-            for linha in conteudo:
-                novoBuffer.append(linha)                                                     #Copia conteudo do include para o buffer
-    novoBuffer.extend(buffer)
-    return novoBuffer
+            conteudo = leArquivo(arquivo)                               #Pega conteudo do arquivo com includes
+            arquivo.close()                                             #Fecha arquivo
+            for linha in conteudo:                                      #Para cada linha do arquivo
+                novoBuffer.append(linha)                                #Copia conteudo do include para o buffer
+    novoBuffer.extend(buffer)                                           #Adiciona o conteudo no inicio do codigo
+    return novoBuffer                                                   #Retorna o novo codigo
 
-def trataDefine(codigo):
+def trataDefine(codigo):                #
     #Ver quais são os defines
     #jogar instruções de define em um vetor
 
@@ -129,33 +139,24 @@ def trataDefine(codigo):
             buffer.append(linha)
     return buffer                                                                          #retorna o código com as alterações
 
-def preprocessa(buffer):    #Função para pre-processar o codigo
-    
+def preprocessa(buffer):                #Função para pre-processar o codigo
+    #buffer = fazIncludes(buffer)#Todos os includes feitos
+
     buffer = listaIncludeAspas(buffer)
 
     buffer = listaIncludeAngular(buffer)
 
     bufferStrings = []
-    def tiraStrings(linha):                                                 #Função para camuflar strings e não quebrar outras funções
+    def mascaraStrings(linha): #Função para camuflar strings e não quebrar outras funções
         string = re.search("\".*\"", linha)
         if string:
             bufferStrings.append(string.group().replace("\\", "\\\\"))
         return re.sub("\".*\"", "#str"+str(len(bufferStrings)-1), linha)
-    buffer = list(map(tiraStrings, buffer))
-
-    '''Para cada linha do código, se a linha contem um define, caso o define seja uma função, irá remover as chaves, caso tenha, e depois irá percorrer o código novamente.
-    Se Achar alguma ocorrência do define no código, irá pegar a expressão, substituir os valores nas variáveis, e então substituir a chamada do define pela expressão.
-    Depois, adicionará a linha em um buffer, que posteriormente será retornado.
-    Caso não seja uma função, apenas substituirá o valor onde houver a a chamada. Depois, adicionará a linha alterada no buffer.
-    Após percorrer todo o código, retornará o buffer'''
-
-    #buffer = trataDefine(buffer)                       #Trata os Defines
+    buffer = list(map(mascaraStrings, buffer))
 
     def tiraComentarioLinha(linha):     #Funcao para remover comentarios de linha
-        #Substitui comentario de linha por "" usando regex e retorna
-        return re.sub("//.*$", "\n", linha)
-    #Para cada linha, se a linha tem // valido(fora de "" validas), apaga conteudo até \n
-    buffer = list(map(tiraComentarioLinha, buffer))   #Remove comentario do tipo "//" de cada linha
+        return re.sub("//.*$", "\n", linha) #Substitui comentario de linha por "" usando regex e retorna
+    buffer = list(map(tiraComentarioLinha, buffer))   #Para cada linha, se a linha tem // valido(fora de "" validas), apaga conteudo até \n
 
     global apagarLinha
     apagarLinha = False
@@ -180,35 +181,37 @@ def preprocessa(buffer):    #Função para pre-processar o codigo
         return linha
     buffer = list(map(tiraComentarioParagrafo, buffer))  #Remove comentario do tipo "/*"
 
+    '''Para cada linha do código, se a linha contem um define, caso o define seja uma função, irá remover as chaves, caso tenha, e depois irá percorrer o código novamente.
+    Se Achar alguma ocorrência do define no código, irá pegar a expressão, substituir os valores nas variáveis, e então substituir a chamada do define pela expressão.
+    Depois, adicionará a linha em um buffer, que posteriormente será retornado.
+    Caso não seja uma função, apenas substituirá o valor onde houver a a chamada. Depois, adicionará a linha alterada no buffer.
+    Após percorrer todo o código, retornará o buffer'''
+    buffer = trataDefine(buffer)                       #Trata os Defines
+
     def tiraEspacos(linha):     #Funcao para remover espaços não uteis
-        #Substitui espaços não uteis por "" usando regex e retorna
-        return re.sub("\s+(?=[-+*\/<>=,&|!(){}\[\];:])|(?<=[-+*\/<>=,&|!(){}\[\];:])\s+(?!=\\n)", "", linha)
-    #Para cada linha, remove espaços em volta de caracteres especiais
-    buffer = list(map(tiraEspacos, buffer))
+        return re.sub("\s+(?=[-+*\/<>=,&|!(){}\[\];:])|(?<=[-+*\/<>=,&|!(){}\[\];:])\s+(?!=\\n)", "", linha)#Substitui espaços não uteis por "" usando regex e retorna
+    buffer = list(map(tiraEspacos, buffer))#Para cada linha, remove espaços em volta de caracteres especiais
 
     def tiraTabulacoes(linha):
         return re.sub("\t", "\n", linha)
-    #Para cada linha, remove tabulações
-    buffer = list(map(tiraTabulacoes, buffer))
+    buffer = list(map(tiraTabulacoes, buffer))#Para cada linha, remove tabulações
     
     def tiraQuebras(linha):     #Função para remover quebras de linha
         #Substitui quebras de linha por "" usando regex e retorna
         return re.sub("\\n", "", linha)
-    #Para cada linha, remove o ultimo "\n"
-    buffer = list(map(tiraQuebras, buffer))           #Remove "\n" de cada linha, defines ja estarão resolvidos
+    buffer = list(map(tiraQuebras, buffer))           #Para cada linha, remove o ultimo "\n"
 
-    def botaStrings(linha):
+    def desmascaraStrings(linha):
         string = re.search("(?<=#str)\d*", linha)
         if string:
             return re.sub("#str\d*", bufferStrings[int(string.group())], linha)
         return linha
-    buffer = list(map(botaStrings, buffer))
+    buffer = list(map(desmascaraStrings, buffer))
     
     buffer = resolveIncludeAspas(buffer)
 
     buffer = resolveIncludeAngular(buffer)
 
-    bufferStrings.clear()                       #Limpa buffer de strings
     return buffer                               #Retorna conteudo após manipulação
 
 os.system("mkdir backup")                               #Cria pasta de backup
@@ -226,9 +229,9 @@ for nomeArquivo in nomesArquivos:                       #Faz o pre-processamento
         codigo = arquivo.readlines()                    #Pega o conteudo do arquivo
         arquivo.close()                                 #Fecha o arquivo
         codigo = preprocessa(codigo)                    #Faz o pre-processa do codigo
-        incluidos.clear()
-        includesAspas.clear()
-        includesAngulares.clear()
+        incluidos.clear()                               #Limpa lista de arquivos incluidos
+        includesAspas.clear()                           #Limpa lista de includes de Aspas
+        includesAngulares.clear()                       #Limpa lista de includes de Colchetes Angulares
         arquivo = open(nomeArquivo, 'w')                #Abre arquivo para escrita
         arquivo.writelines(codigo)                      #Escreve o conteudo no arquivo
         arquivo.close()                                 #Fecha o arquivo
